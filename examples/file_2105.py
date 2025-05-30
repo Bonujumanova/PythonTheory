03508cd8c47342c6323ff0ef79e9cbcc3d572729
Shoxrux Yuldashov <deagleshox@mail.ru>
2025-05-30 06:51:38 +0100

    Теория: Аннотации типов в Python

---
+++ examples/file_2105.py
@@ -0,0 +1,18 @@
+from typing import Optional
+
+a: bool = True
+
+s: str = "Hello"
+
+b: float = 5.125
+
+d: int = 4254
+
+name: None | str | int = None
+name_2: Optional[str, int] = None
+
+a = "ssdgd"
+
+print(name)
+
+name = 53.23
