dd835ddbbd2b39f76e5428e737819143c14cbe66
Shoxrux Yuldashov <deagleshox@mail.ru>
2025-06-27 06:59:35 +0100

    Практика: Паттерны с помощью циклов (вложенных)
    - Butterfly
    - Hourglass

---
+++ loop_pattern/butterfly_pattern.py
@@ -0,0 +1,21 @@
+n: int = 10
+
+for i in range(1, n + 1):
+    for j in range(i):
+        print("*", end=' ')
+    for q in range((n * 2) - (i * 2)):
+        print(' ', end= ' ')
+    for k in range(i):
+        print('*', end=' ')
+    print()
+
+for i in range(n - 1, 0, -1):
+    for j in range(i):
+        print('*', end=' ')
+    for q in range((n * 2) - (i * 2)):
+        print(' ', end=' ')
+    for k in range(i):
+        print('*', end=' ')
+    print()
+
+
---
+++ loop_pattern/hourglass.py
@@ -0,0 +1,11 @@
+n: int = 13
+
+for i in range(1, n, 2):
+    for j in range(n // 2 + 1 - i):
+        if i <= n // 2 + 1:
+            print('*', end=' ')
+        else:
+            
+
+
+
