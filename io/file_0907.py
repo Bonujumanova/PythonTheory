2b3622a83a223cdd5c45bdc81d92ad0f63d4c654
Shoxrux Yuldashov <deagleshox@mail.ru>
2025-07-09 07:08:44 +0100

    Теория: 3.5. Потоковый ввод/вывод. Работа с текстовыми файлами.
    - Добавлены примеры

--- io/file_0907.py
+++ io/file_0907.py
@@ -1,6 +1,10 @@
+from pprint import pprint
 import sys
 
 with open("example_0907.txt", mode="w", encoding="UTF-8") as f:
     data = sys.stdin.readlines()
 
     f.writelines(data)
+
+with open("translators_base.txt", mode="r", encoding="UTF-8") as f:
+    pprint(f.readlines())
